3e13cedee25bf69cddd66151c17b343becae4344
tohri isnadi <tohri.isnadi@gmail.com>
2020-01-08 07:21:20 +0700

    first commit

---
+++ php/contact_form.php
@@ -0,0 +1,24 @@
+<?php
+	$emailTo = "vcard@sitename.com"; // Enter your email for feedbacks here
+	
+	$headers = "MIME-Version: 1.0\r\n";
+	$headers .= "Content-type: text/html; charset=utf-8\r\n";
+	$headers .= "From: ".$_GET['email']."\r\n";
+	
+	if (!isset($_GET['subject'])) {
+		$subject = "Contact form message"; // Enter your subject here
+	} else {
+		$subject = $_GET['subject'];
+	}
+	$body = "";
+
+	reset($_GET);
+	while (list($key, $val) = each($_GET)) {
+		$title = ucwords(strtolower($key));
+		$body .= "<b>$title:</b> ";
+		$body .= $val;
+	  	$body .= "<br>";
+	}
+	  
+	$result = mail($emailTo, $subject, $body, $headers);
+?>
